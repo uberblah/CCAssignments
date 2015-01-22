bc2d4ca6f5233ee0f29d3d2ea87c1715f54d7dce
fisherdj <fisherdj@users.noreply.github.com>
2015-01-22 20:38:23 +0000

    finished compiler?

--- 1/compile/compile.py
+++ 1/compile/compile.py
@@ -6,6 +6,7 @@ import sys
 
 #print compiler.parse(sys.stdin.read())
 
+#TODO: move some of this stuff outside of compile.py
 def par(n):
 	return "(" + str.join(" ", filter(len,n)) + ")"
 
@@ -78,7 +79,7 @@ def getnames(n): # get full set of names used by program so that we can assign t
 	else:
 		return []
 
-# instead of dealing with 
+# instead of dealing with Python AST nodes directly, translate expressions into this format
 def exprEval(n,loc):
 	if isinstance(n, Const):
 		return [['=lit',n.value,loc]]
@@ -89,7 +90,7 @@ def exprEval(n,loc):
 	elif isinstance(n, Name):
 		return [['=name',n.name,loc]]
 	elif isinstance(n, CallFunc):
-		return [['call']] # fill this out more later
+		return [['call',loc]] # fill this out more later
 
 def discardEval(n):
 	if isinstance(n, CallFunc):
@@ -120,23 +121,73 @@ def stmtEval(n):
 def progEval(n):
 	return stmtEval(n.node)
 
+def getIRtmp(n):
+	ndict = {} # TODO: translation dictionary in separate file
+	ndict['=lit'] = lambda x: x[2]
+	ndict['+'] = lambda x: max(x[1], x[2])
+	ndict['-'] = lambda x: x[1]
+	ndict['=name'] = lambda x: x[2]
+	ndict['print'] = lambda x: 1
+	ndict['name='] = lambda x: x[1]
+	ndict['call'] = lambda x: x[1]
+	if not n:
+		return 0
+	#print map(lambda x: ndict[x[0]](x),n)
+	return max(map(lambda x: ndict[x[0]](x),n))
+
 def compileIR(n,ndict,ldict):
-	tdict = {}
-	tdict['=lit'] = lambda x: "movl $" + str(n[1]) + ", " + ldict[n[2]]
-	tdict['+'] = lambda x: "addl " + ldict[n[1]] + ", " + ldict[n[2]]
-	tdict['-'] = lambda x: "negl " + ldict[n[1]]
-	tdict['=name'] = lambda x: "movl " + ndict[n[1]] + ", " + ldict[n[2]]
-	tdict['print'] = lambda x: "pushl " + ldict[n[0]] + "\ncall print_int_nl\naddl $4, %esp"
-	tdict['name='] = lambda x: "movl " + ldict[n[1]] + ", " + ndict[n[2]]
-	tdict['call'] = lambda x: "call input" #"pushl %eax\npushl %ebx\npushl %edx"
-	return str.join("\n",map(lambda x: tdict[x[0]](x)))
+	tdict = {} # TODO: translation dictionary in separate file
+	tdict['=lit'] = lambda x: "movl $" + str(x[1]) + ", " + ldict[x[2]]
+	tdict['+'] = lambda x: "movl " + ldict[x[2]] + ", %eax\naddl %eax, " + ldict[x[2]]
+	tdict['-'] = lambda x: "negl " + ldict[x[1]]
+	tdict['=name'] = lambda x: "movl " + ndict[x[1]] + ", %eax\nmovl %eax, " + ldict[x[2]]
+	tdict['print'] = lambda x: "call print_int_nl" #"movl " + ldict[0] + ldict[x[1]] + "\ncall print_int_nl"#\naddl $4, %esp"
+	tdict['name='] = lambda x: "movl " + ldict[x[1]] + ", %eax\nmovl %eax, " + ndict[x[2]]
+	tdict['call'] = lambda x: "call input\nmovl %eax, " + ldict[x[1]] #"pushl %eax\npushl %ebx\npushl %edx"
+	return str.join("\n",map(lambda x: tdict[x[0]](x),n))
+
+simpleHead = '''.global main
+main:
+pushl %ebp
+movl %esp, %ebp'''
+
+def compile(n):
+	def genHeader(stacksize):
+		stackMake = "subl $" + str(4*stacksize) + ", %esp"
+		return simpleHead + "\n" + stackMake
+	def genDict(names,tmpsize):
+		ret={}
+		spot = tmpsize
+		for i in names:
+			ret[i] = str(spot*4)+"(%esp)"
+			spot = spot + 1
+		return ret
+	def genTDict(names,tmpsize):
+		ret={}
+		spot = 0
+		for i in range(0, tmpsize+1):
+			ret[i] = str(spot*4)+"(%esp)"
+			spot = spot + 1
+		return ret
+	ast = parse(n)
+	names = getnames(ast)
+	ir = progEval(ast)
+	tmpsize = getIRtmp(ir)
+	stacksize = tmpsize + len(names)
+	head = genHeader(stacksize)
+	ndict = genDict(names,tmpsize)
+	ldict = genTDict(names,tmpsize)
+	foot = "leave\nret\n"
+	return head + "\n" + compileIR(ir,ndict,ldict) + "\n" + foot
 
 def le(n):
-	return progEval(parse(n))
+	return compile(n)
+	#return progEval(parse(n))
 	#return lispexpr(unwrap(parse(n)))
 	#return unexpr(parse(n))
 	#return getnames(parse(n))
 
+'''
 print "1:"
 print le("")
 print "2:"
@@ -144,15 +195,25 @@ print le("2+2")
 print "3:"
 print le("a=2+2")
 print "4:"
-print le("a=2+2\n2+2+2\nprint a")
+print le("a=2+2\n2+2+2+input()\nprint a")
+print "5:"
+print le("a=(2+2+(- 5))+(input()+5+2)\n2+2+2+input()\nprint a")
+print "6:"
+print le("print input() + input()")
+print "7:"
+print le("x = - input()\nprint x + input()")
+'''
 
-f = sys.argv[1]
-l = len(f)
-if f[l-1] == 'y' and f[l-2] == 'p' and f[l-3] == '.' :
-	f = f[0:l-3] + '.s'
+r = sys.argv[1]
+l = len(r)
+if r[l-1] == 'y' and r[l-2] == 'p' and r[l-3] == '.' :
+	f = r[0:l-3] + '.s'
 else:
-	f = f + '.s'
+	f = r + '.s'
+r = open(r,'r')
 f = open(f,'w')
+
+f.write(compile(r.read()))
 
 """
 f.write('''.global main
@@ -164,6 +225,7 @@ ret
 ''')
 """
 
+"""
 f.write('''.globl main
 main:
 call input
@@ -172,6 +234,7 @@ call print_int_nl
 addl $4, %esp
 ret
 ''')
+"""
 
 '''
 print tablines(tablines("notab\n\tonetab\n\t\ttwotab\nnotabagain"))
