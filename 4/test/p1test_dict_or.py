b00fd7585f1b070a411ad8e06aa2b56562f9eaea
uberblah <miles.rufatlatre@colorado.edu>
2015-02-28 17:21:35 -0700

    LOTS OF TESTS

---
+++ 4/test/p1test_dict_or.py
@@ -0,0 +1,9 @@
+a = {0:1, 1:2, 2:3}
+b = {3:4, 4:5, 5:6}
+c = a or b
+print c[0]
+print c[1]
+print c[2]
+print c[3]
+print c[4]
+print c[5]
---
+++ 4/test/p1test_if_basic.py
@@ -0,0 +1,2 @@
+print 1 if input() else 2
+print 1 if input() else 2
